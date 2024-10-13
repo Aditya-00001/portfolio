74bdd566cc07cd04b6bd43f3a214ca572cff17dc
Aditya-00001 <130139603+Aditya-00001@users.noreply.github.com>
2024-10-13 22:47:35 +0530

    Update backend_portfolio.py

--- backend_portfolio.py
+++ backend_portfolio.py
@@ -34,12 +34,18 @@ def index():
 def project():
     projects = [
         {
-            'title': ' Robust biometric attendance system via content-based '
-                     'feature extraction and classification of acknowledged sound',
-            'description': 'Biometric attendance systems have simplified tracking but still present challenges, such as users not being assured their attendance is recorded. A novel method using machine learning addresses this by analyzing thank-you sounds from biometric devices. The system applies signal processing techniques like Mel-frequency cepstral coefficients (MFCC) and Mel spectrograms to distinguish genuine acknowledgments from noise. This scalable and versatile solution integrates seamlessly with existing systems, automating attendance tracking while providing users with confirmation that their attendance has been recorded.',
-            'link': None,  # This project has a link
-            'image': url_for('static',filename='projects/attendance.png'),  # Replace with actual image URL
-            'skills': ['Python', 'App Development','Machine Learning']  # Add skills related to the project
+            'title': 'Robust biometric attendance system via content-based feature extraction and classification of acknowledged sound',
+            'description': 'Biometric attendance systems have simplified tracking but still present challenges, '
+                           'such as users not being assured their attendance is recorded. A novel method using '
+                           'machine learning addresses this by analyzing thank-you sounds from biometric devices. '
+                           'The system applies signal processing techniques like Mel-frequency cepstral coefficients '
+                           '(MFCC) and Mel spectrograms to distinguish genuine acknowledgments from noise. This '
+                           'scalable and versatile solution integrates seamlessly with existing systems, automating '
+                           'attendance tracking while providing users with confirmation that their attendance has '
+                           'been recorded.',
+            'link': None,  # This project has no link
+            'icon': 'fas fa-user-check',  # Font Awesome icon class
+            'skills': ['Python', 'App Development', 'Machine Learning']  # Add skills related to the project
         },
         {
             'title': 'Instagram Automation',
@@ -47,21 +53,21 @@ def project():
                            'Instagram can be a daunting task. To address this challenge, I developed an Instagram '
                            'Automation Tool that leverages Python and Selenium to automate key interactions, '
                            'enabling users to focus on what truly matters—creating engaging content.',
-            'link': 'https://github.com/Aditya-00001/Instagram-Automation',  # This project does not have a link
-            'image': url_for('static',filename='projects/instabot.png'),  # Replace with actual image URL
+            'link': 'https://github.com/Aditya-00001/Instagram-Automation',  # This project has a link
+            'icon': 'fab fa-instagram',  # Font Awesome icon class
             'skills': ['Python', 'Selenium', 'HTML', 'CSS']  # Add skills related to the project
         },
         {
             'title': 'Morse Code converter',
             'description': 'The Text to Morse Code Converter is a command-line interface (CLI) application designed to '
-                           'facilitate the conversion of text into Morse code and vice versa. T'
-                           'his project aims to provide users with an intuitive tool for encoding and decoding messages '
-                           'using the universal language of Morse code.',
-            'link': 'https://github.com/Aditya-00001/python-professional/blob/main/textToMorse.py',  # This project does not have a link
-            'image': url_for('static', filename='projects/morse.png'),  # Replace with actual image URL
+                           'facilitate the conversion of text into Morse code and vice versa. This project aims to '
+                           'provide users with an intuitive tool for encoding and decoding messages using the universal '
+                           'language of Morse code.',
+            'link': 'https://github.com/Aditya-00001/python-professional/blob/main/textToMorse.py',
+            # This project has a link
+            'icon': 'fas fa-code',  # Font Awesome icon class
             'skills': ['Python']  # Add skills related to the project
         },
-        # Add more projects as needed
     ]
 
     return render_template('project.html', projects=projects)
@@ -81,6 +87,7 @@ def contact():
         with SMTP("smtp.gmail.com", 587) as connection:
             connection.starttls()
             connection.login(user=my_email, password=password)
+
             connection.sendmail(from_addr=my_email,to_addrs=send,
                                 msg=f"Subject: Portfolio Message from {email}\n\n By- {email}\n phone - {cont}\nMessage:\n{msg}")
             connection.close()
